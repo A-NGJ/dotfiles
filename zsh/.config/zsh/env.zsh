2fde45c4397971803e9429075cc31ade5d39dd00
Aleksander Nagaj <aleksander.nagaj@netlight.com>
2026-01-22 12:39:25 +0100

    feat(zsh): add .local/bin to PATH

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -5,6 +5,7 @@ prepend '/usr/local/bin'
 prepend '/opt/homebrew/bin'
 prepend '/opt/homebrew/sbin'
 prepend "$HOME/bin"
+prepend "$HOME/.local/bin"
 prepend '/home/linuxbrew/.linuxbrew/bin'
 unset prepend
 export PATH
