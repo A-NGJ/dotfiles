182bb867d12d4ead5f10fd528e8b1b46b636633b
Aleksander Nagaj <aleksander.nagaj@netlight.com>
2026-02-06 12:27:42 +0100

    refactor(nvim): simplify noice to cmdline popup only
    Re-enable noice.nvim but disable all features except the cmdline popup.
    Keep the centered floating command input (row 5, width 60) while
    disabling messages, notifications, popupmenu, and all LSP UI elements.
    
    Removes nvim-notify dependency as it's no longer needed.

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,73 +1,30 @@
--- return {
---     "folke/noice.nvim",
---     config = function()
---         require("noice").setup({
---             -- add any options here
---             routes = {
---                 {
---                     filter = {
---                         event = 'msg_show',
---                         any = {
---                             { find = "written" },
---                             { find = '%d+L, %d+B' },
---                             { find = '; after #%d+' },
---                             { find = '; before #%d+' },
---                             { find = '%d fewer lines' },
---                             { find = '%d more lines' },
---                         },
---                     },
---                     opts = { skip = true },
---                 },
---                 {
---                     filter = {
---                         event = "notify",
---                         find = "No information available",
---                     },
---                     opts = {
---                         skip = true,
---                     },
---                 },
---             },
---             views = {
---                 cmdline_popup = {
---                     position = {
---                         row = 5,
---                         col = "50%",
---                     },
---                     size = {
---                         width = 60,
---                         height = "auto",
---                     },
---                 },
---                 popupmenu = {
---                     relative = "editor",
---                     position = {
---                         row = 8,
---                         col = "50%",
---                     },
---                     size = {
---                         width = 60,
---                         height = 10,
---                     },
---                     border = {
---                         style = "rounded",
---                         padding = { 0, 1 },
---                     },
---                     win_options = {
---                         winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
---                     },
---                 },
---             },
---             messages = {
---                 enabled = false,
---             }
---         })
---     end,
---     dependencies = {
---         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
---         "MunifTanjim/nui.nvim",
---         "rcarriga/nvim-notify",
---     }
--- }
-
-return {}
+return {
+    "folke/noice.nvim",
+    dependencies = {
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        cmdline = { enabled = true },
+        messages = { enabled = false },
+        popupmenu = { enabled = false },
+        notify = { enabled = false },
+        lsp = {
+            progress = { enabled = false },
+            message = { enabled = false },
+            hover = { enabled = false },
+            signature = { enabled = false },
+        },
+        views = {
+            cmdline_popup = {
+                position = {
+                    row = 5,
+                    col = "50%",
+                },
+                size = {
+                    width = 60,
+                    height = "auto",
+                },
+            },
+        },
+    },
+}
