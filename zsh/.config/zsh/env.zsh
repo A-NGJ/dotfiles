570933495f10e84ba56956c8c56b1b2ecc001cf0
A-NGJ <anagaj99@gmail.com>
2026-03-13 13:50:55 +0100

    chore(lsp): add mason lsp bin to PATH if it exists

--- zsh/.config/zsh/env.zsh
+++ zsh/.config/zsh/env.zsh
@@ -8,6 +8,11 @@ prepend "$HOME/bin"
 prepend "$HOME/.local/bin"
 prepend '/home/linuxbrew/.linuxbrew/bin'
 prepend "$(go env GOPATH)/bin"
+
+MASON_BIN="$HOME/.local/share/nvim/mason/bin"
+[ -d "$MASON_BIN" ] && prepend "$MASON_BIN"
+
 unset prepend
+
 export PATH
 
